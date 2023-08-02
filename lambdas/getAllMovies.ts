dbd7a3162a8331b417361cf9ddcadc0b99764d98
Diarmuid O Connor <doconnor@wit.ie>
2023-08-02 17:01:52 +0100

    Lambda function to get all movies

---
+++ lambdas/getAllMovies.ts
@@ -0,0 +1,63 @@
+import { Handler } from "aws-lambda";
+
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+
+export const handler: Handler = async (event, context) => {
+  try {
+    // Print Event
+    console.log("Event: ", event);
+
+    const marshallOptions = {
+      convertEmptyValues: true,
+      removeUndefinedValues: true,
+      convertClassInstanceToMap: true,
+    };
+    const unmarshallOptions = {
+      wrapNumbers: false,
+    };
+    const translateConfig = { marshallOptions, unmarshallOptions };
+    const ddbDocClient = DynamoDBDocumentClient.from(
+      ddbClient,
+      translateConfig
+    );
+    const commandOutput = await ddbDocClient.send(
+      new ScanCommand({
+        TableName: process.env.TABLE_NAME,
+      })
+    );
+    console.log('ScanCommand response: ', commandOutput)
+    if (!commandOutput.Items) {
+      return {
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Invalid movie Id" }),
+      };
+    }
+    const body = {
+      data: commandOutput.Items,
+    };
+
+    // Return Response
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error }),
+    };
+  }
+};
